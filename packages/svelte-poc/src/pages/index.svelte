adbe56fadddbaa003e890a2760e108ab7e6f1413
zadubanm02 <zadubanm02@gmail.com>
2020-08-07 17:18:48 +0200

    #tabulka v sharing profile data

--- packages/svelte-poc/src/pages/index.svelte
+++ packages/svelte-poc/src/pages/index.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { hoveredNodeId, rootNodes, profilerEnabled } from '../store.js'
+  import { hoveredNodeId, rootNodes, profilerEnabled } from "../store.js";
   // import Toolbar from '../toolbar/Toolbar.svelte'
   // import Search from '../toolbar/Search.svelte'
   // import ProfileButton from '../toolbar/ProfileButton.svelte'
@@ -8,19 +8,19 @@
   // import ComponentView from '../panel/ComponentView.svelte'
   // import Profiler from '../profiler/Profiler.svelte'
   // import ConnectMessage from '../ConnectMessage.svelte'
-  import Node from '../nodes/Node.svelte'
+  import Node from "../nodes/Node.svelte";
   // import '../client/index'
 
   //window.document.addEventListener
 </script>
 
 <b>ahoj</b>
-{#each ['A','B','C'] as item}
-<div>{item}</div>
-<!--{#if item == 'B'}
+{#each ['A', 'B', 'C'] as item}
+  <div>{item}</div>
+  <!--{#if item == 'B'}
   nasiel som
   {/if}
-  <div>{item}</div>
+  <div>{item}</div>a
   {#if true}
      vnturi <p>if</p>
   {/if}
