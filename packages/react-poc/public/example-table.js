b895f9dc40949307f411ec76d48feda9ce838736
zadubanm02 <zadubanm02@gmail.com>
2020-08-10 17:27:38 +0200

    WIP Add Column to a specific position

--- packages/react-poc/public/example-table.js
+++ packages/react-poc/public/example-table.js
@@ -1,6 +1,5 @@
 const { Fragment, Suspense, useState } = React;
 const { render } = ReactDOM;
-
 function App() {
     return (
         <Suspense fallback={<Loading />}>
@@ -36,40 +35,78 @@ function Table() {
         border: '5px solid pink'
     };
     const buttonAdd = {
-        position: "absolute",
+        position: 'absolute',
         borderRadius: 50,
         opacity: 0.3,
-        zIndex: 1000
     };
 
 
+    function checkIndex(e) {
+        const table = document.querySelector('table');
+        const rows = document.querySelectorAll('tr');
+        const rowsArray = Array.from(rows);
 
+        table.addEventListener('click', (event) => {
+            const rowIndex = rowsArray.findIndex(row => row.contains(event.target));
+            const columns = Array.from(rowsArray[rowIndex].querySelectorAll('td'));
+            const columnIndex = columns.findIndex(column => column == event.target);
+            console.log(rowIndex, columnIndex)
+        })
 
-    const tableStyle = {
-        borderRight: "solid rgb(124, 144, 236, 0.5)"
     }
 
-    const addStylesToColumn = () => {
-        const body = document.querySelector('.table').tBodies[0];
-        document.querySelector('tr').lastElementChild.style.borderRight = "solid blue"
-        for (let rows = 0; rows < body.rows.lenght; rows++) {
-            body.rows[rows].style.borderRight = "solid blue"
+
+    let target = null
+    function handleMousemove(e) {
+        target = e.target
+        highlightFun({ type: 'element', detail: target })
+        console.log("Target", target)
+        console.log("Detail", e.detail)
+        setAdd(true)
+    }
+
+    function handleAddCol(e) {
+        target = e.target;
+        const table = document.querySelector('table');
+        const rows = document.querySelectorAll('tr');
+        const rowsArray = Array.from(rows);
+        const rowIndex = rowsArray.findIndex(row => row.contains(e.target));
+        const columns = Array.from(rowsArray[rowIndex].querySelectorAll('th'));
+        const columnIndex = columns.findIndex(column => column == e.target);
+        console.log(rowIndex, columnIndex)
+        e.target.addEventListener('click', () => {
+            const head = document.querySelector('.table').tHead;
+            const newEl = document.createElement('th');
+            newEl.innerHTML = "NewHead"
+            console.log('New El', newEl)
+            target.after(newEl)
+            const tableBody = document.querySelector('.table').tBodies[0];
+            for (let i = 0; i < tableBody.rows.length; i++) {
+                let newCell = document.createElement('td')
+                newCell.innerHTML = 'NewTD'
+                const afterThis = tableBody.rows[i].cells[columnIndex]
+                console.log('After this', afterThis)
+                afterThis.after(newCell)
+            }
+        })
+
+    }
+
+    function addSelectedColumn() {
+        const head = document.querySelector('.table').tHead;
+        console.log("head", head)
+        for (let h = 0; h < head.rows.length; h++) {
+            let newTH = document.createElement('th');
+            head.rows[h].appendChild(newTH);
+            newTH.innerHTML = "New"
+        }
+        const tableBody = document.querySelector('.table').tBodies[0];
+        for (let i = 0; i < tableBody.rows.length; i++) {
+            let newCell = tableBody.rows[i].insertCell(-1);
+            newCell.innerHTML = "New Cell"
         }
     }
 
-    const deleteStyle = () => {
-        document.querySelector('tr').lastElementChild.style.color = "#000"
-    }
-
-    const createDiv = () => {
-        const createdDiv = document.createElement('div')
-        createdDiv.style.zIndex = 1000;
-        createdDiv.style.borderRight = "solid blue"
-        createdDiv.style.color = "blue"
-        createdDiv.style.position = ""
-        createdDiv.innerText = "CREATED DIV"
-        document.querySelector('th').lastElementChild.append(createdDiv)
-    }
 
     const addColumn = () => {
         const head = document.querySelector('.table').tHead;
@@ -84,51 +121,55 @@ function Table() {
             let newCell = tableBody.rows[i].insertCell(-1);
             newCell.innerHTML = "New Cell"
         }
+
     }
     return (
-        <div className="container">
-            <h2 className="text-center">UzitocnePage</h2>
-            <div>
-                <table class="table" >
-                    <thead onMouseEnter={() => { setAdd(true); }} onMouseLeave={() => { setAdd(false) }}>
-                        <tr>
-                            <th scope="col">#</th>
+        <div>
+            <div className="container" >
+                <h2 className="text-center">UzitocnePage</h2>
+                <div>
+                    <table class="table" >
+                        <thead onClick={(e) => { handleAddCol(e) }} onMouseEnter={(e) => { handleMousemove(e); checkIndex(e) }} onMouseLeave={() => { setAdd(false) }}>
+                            <tr>
+                                <th scope="col">#</th>
 
-                            <th scope="col" onMouseEnter={() => { setIsShown(true) }} onMouseLeave={() => { setIsShown(false) }}> {edit ? null : columnData}
-                                {show ? <button className="btn btn-primary btn-sm" style={buttonAdd} onClick={() => { setEdit(true); setIsShown(false) }}><i class="fas fa-edit"></i></button> : null}{edit ?
-                                    <div><input type="text" name="columnData" value={columnData}
-                                        onChange={(e) => { setColumnData(e.target.value) }}
-                                    /> <button className="btn btn-primary btn-sm" onClick={() => { console.log("hey"); setEdit(false); setIsShown(false) }}>Save</button> </div> : null}
-                            </th>
-                            <th scope="col">Last</th>
-                            <th scope="col" >Handle</th>
 
-                            {add ? <button className="btn btn-primary btn-sm" style={buttonAdd} onClick={addColumn}><i class="fas fa-plus"></i></button> : null}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td >@mdo</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td >@fat</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Larry</td>
-                            <td>the Bird</td>
-                            <td >@twitter</td>
-                        </tr>
-                    </tbody>
-                </table>
+                                <th scope="col" onMouseEnter={() => { setIsShown(true) }} onMouseLeave={() => { setIsShown(false) }}> {edit ? null : columnData}
+                                    {show ? <button className="btn btn-primary btn-sm" style={buttonAdd} onClick={() => { setEdit(true); setIsShown(false) }}><i class="fas fa-edit"></i></button> : null}{edit ?
+                                        <div><input type="text" name="columnData" value={columnData}
+                                            onChange={(e) => { setColumnData(e.target.value) }}
+                                        /> <button className="btn btn-primary btn-sm" onClick={() => { console.log("hey"); setEdit(false); setIsShown(false) }}>Save</button> </div> : null}
+                                </th>
+                                <th scope="col">Last</th>
+                                <th scope="col" >Handle</th>
+
+                                {add ? <button className="btn btn-primary btn-sm" style={buttonAdd} onClick={addColumn}><i class="fas fa-plus"></i></button> : null}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td scope="row">1</td>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td >@mdo</td>
+                            </tr>
+                            <tr>
+                                <td scope="row">2</td>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td >@fat</td>
+                            </tr>
+                            <tr>
+                                <td scope="row">3</td>
+                                <td>Larry</td>
+                                <td>the Bird</td>
+                                <td >@twitter</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </div>
+        </div >
     )
 
 
