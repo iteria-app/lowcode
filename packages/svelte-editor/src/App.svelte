7d60c560ad8541678321fb58292189626967eb2f
jslezak <jozef.slezak@gmail.com>
2020-08-14 09:59:21 +0200

    stacktrace.js initial commit

---
+++ packages/svelte-editor/src/App.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { Router } from "@sveltech/routify";
+  import { routes } from "../.routify/routes";
+</script>
+
+<style  global>
+  @import "../static/global.css";
+</style>
+
+<Router {routes} />
---
+++ packages/svelte-editor/src/pages/index.svelte
@@ -0,0 +1,8 @@
+<script>
+    import RoutifyIntro from './example/_components/RoutifyIntro.svelte'
+    import { metatags } from '@sveltech/routify'
+    metatags.title = 'My Routify app'
+    metatags.description = 'Description coming soon...'
+</script>
+KUKIIIK
+<RoutifyIntro />
