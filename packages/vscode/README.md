e1b7e913e7823cf0e41e488131e32418ef8edd2d
jslezak <jozef.slezak@gmail.com>
2020-08-21 09:17:54 +0200

    initial commit - workin progress for vscode plugin

---
+++ packages/vscode/README.md
@@ -0,0 +1,6 @@
+# Svelte Code Tree Editor
+
+## Credits
+- JSON Tree Editor https://github.com/josdejong/jsoneditor
+- Prettier
+- Prettier Svelte
---
+++ packages/vscode/out/src/globals/enums.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.configurationSettings = void 0;
+var configurationSettings;
+(function (configurationSettings) {
+    configurationSettings["theme"] = "theme";
+})(configurationSettings = exports.configurationSettings || (exports.configurationSettings = {}));
+//# sourceMappingURL=enums.js.map
