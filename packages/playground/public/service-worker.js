f20c017921e59e2b4683f2de8793625049adc79d
jslezak <jozef.slezak@gmail.com>
2020-11-01 23:14:32 +0100

    service worker calling svelte compiler

--- packages/playground/public/service-worker.js
+++ packages/playground/public/service-worker.js
@@ -1,97 +1,168 @@
-const CACHE = 'previewB'
-const CONTROLLED = '/controlled/'
+//const CACHE = 'previewB'
+//const CONTROLLED = '/controlled/'
+const appSvelte = `<script>
+import Nested from './Nested.svelte';
+</script>
 
-console.log('service wokeeeer')
-
-self.addEventListener('install', function(event) {
-  console.log('The service worker is being installed...', event);
-  event.waitUntil(self.skipWaiting()); // Activate worker immediately
-  // Open a cache and use `addAll()` with an array of assets to add all of them
-  // to the cache. Ask the service worker to keep installing until the
-  // returning promise resolves.
-  //event.waitUntil(caches.open(CACHE).then(function (cache) {
-  //  cache.addAll([
-      //'./index.tsx',
-      //'./index.svelte',
-  //  ]);
-  //}));
-});
-
-
-self.addEventListener('activate', function(event) {
-  console.log('The service worker is being activated...', event);
-  event.waitUntil(self.clients.claim()); // Become available to all pages
-})
-
-// On fetch, use cache but update the entry with the latest contents
-// from the server.
-self.addEventListener('fetch', function(evt) {
-  const url = new URL(evt.request.url)
-  console.log('sw fetch', url.pathname, evt);
-  if (url.pathname.startsWith(CONTROLLED)) {
-    console.log('sw fetch - startsWith ', CONTROLLED)
-
-    // You can use `respondWith()` to answer ASAP...
-    const ch = fromCache(evt.request)
-    console.log('sw ch', ch)
-    evt.respondWith(ch);
-    // ...and `waitUntil()` to prevent the worker to be killed until
-    // the cache is updated.
-    evt.waitUntil(
-      update(evt.request)
-      // Finally, send a message to the client to inform it about the
-      // resource is up to date.
-      .then(refresh)
-    );
-  }
-});
-
-self.addEventListener('message', function (evt) {
-  console.log('postMessage received', evt.data);
-})
-
-// Open the cache where the assets were stored and search for the requested
-// resource. Notice that in case of no matching, the promise still resolves
-// but it does with `undefined` as value.
-function fromCache(request) {
-  return caches.open(CACHE).then(function (cache) {
-    //return cache.match(request);
-    return new Response("<h1>Cauky!</h1>", {
-      headers: {'Content-Type': 'text/html'}
-    })
-  })
+<style>
+p {
+color: purple;
+font-family: 'Comic Sans MS', cursive;
+font-size: 2em;
 }
+</style>
 
-// Update consists in opening the cache, performing a network request and
-// storing the new response data.
-function update(request) {
-  return caches.open(CACHE).then(function (cache) {
-    return fetch(request).then(function (response) {
-      console.log('sw net response', response)
-      return cache.put(request, response.clone()).then(function () {
-        return response;
+<p>These styles...</p>
+<Nested/>`
+const nestedSvelte = `<p>...don't affect this element
+<i>ii</i><b id="ferino">bbb</b></p>`
+
+
+self.window = self; // egregious hack to get magic-string to work in a worker (window.btoa)
+
+console.log('sw run service-worker.js', typeof window)
+
+self.addEventListener('install', function(e) {
+  console.log('sw installing', self.registration)
+  self.skipWaiting();
+});
+
+importScripts('https://unpkg.com/svelte@3.29.4/compiler.js')
+
+/*self.addEventListener('activate', function(e) {
+  console.log('sw activating', self.registration)
+
+  self.registration.unregister()
+    .then(function() {
+      return self.clients.matchAll();
+    })
+    .then(function(clients) {
+      console.log('sw unregistering', clients)
+
+      clients.forEach(client => {
+        console.log('sw unregister => navigate', client.url)
+        client.navigate(client.url)
       });
     });
-  });
+});*/
+
+function content(request) {
+  if (request.url.indexOf('App') >= 0) {
+    return {source: appSvelte, filename: 'App.svelte'}
+  }
+  if (request.url.indexOf('Nested') >= 0) {
+    return {source: nestedSvelte, filename: 'Nested.svelte'}
+  }
+  return null
 }
 
-// Sends a message to the clients.
-function refresh(response) {
-  return self.clients.matchAll().then(function (clients) {
-    clients.forEach(function (client) {
-      // Encode which resource has been updated. By including the
-      // [ETag](https://en.wikipedia.org/wiki/HTTP_ETag) the client can
-      // check if the content has changed.
-      var message = {
-        type: 'refresh',
-        url: response.url,
-        // Notice not all servers return the ETag header. If this is not
-        // provided you should use other cache headers or rely on your own
-        // means to check if the content has changed.
-        eTag: response.headers.get('ETag')
-      };
-      // Tell the client about the update.
-      client.postMessage(JSON.stringify(message));
-    });
-  });
+function respondWith(event, response) {
+  event.respondWith(new Promise((resolve, reject) => { 
+    resolve(response); 
+    reject()  
+  } ))
 }
+
+function sv({source, filename}) {
+  const options = {
+    dev: true,
+    css: false,
+    filename,//DOM element.__svelte_meta: {loc: char: 45, column: 13, file: "Nested.svelte", line: 1}
+    sveltePath: 'https://unpkg.com/svelte@3.29.4'//parse package json version...
+  }
+  //const parsed = svelte.parse(source, options);
+  const compiled = svelte.compile(source, options)
+  console.log('svelte compiled', compiled.js.code)
+
+  return compiled.js.code.replaceAll('https://unpkg.com/svelte@3.29.4/internal', 'https://unpkg.com/svelte@3.29.4/internal/index.mjs')
+}
+
+const svelteExt = '.svelte'
+function svelteExtenstion(filename) {
+  if (filename.endsWith(svelteExt)) {
+    return filename
+  }
+  return filename + svelteExt
+}
+
+function svelteExtenstionToJs(filename) {
+  if (filename.endsWith(svelteExt)) {
+    return filename.substring(0, filename.length - svelteExt.length) + '.js'
+  }
+  return filename + '.js'
+}
+
+function newJavaScriptResponse(content) {
+  const headers = new Headers()
+  headers.append("Content-Type", "application/javascript")
+  const init = { "status" : 200 , "statusText": "OK", headers };
+  return new Response(content, init)
+}
+
+self.addEventListener('fetch', function(event) {
+  const requestURL = new URL(event.request.url)
+  console.log('sw fetch requestURL', requestURL.pathname)
+  const c = content(event.request)
+  if (c) {
+    event.respondWith(caches.open('playground').then(async cache => {
+      let jsMatch = await cache.match(svelteExtenstionToJs(requestURL.pathname))
+      if (jsMatch) {
+        return jsMatch
+      }
+      let svelteMatch = await cache.match(svelteExtenstion(requestURL.pathname))
+      if (!svelteMatch) {
+        cache.put(svelteExtenstion(requestURL.pathname), svelteMatch = newJavaScriptResponse(c.source))
+      }
+      console.log('sw match', svelteMatch)
+      //TODO const text = await svelteMatch.text()//TODO filename => sv({source: text, filename})
+      const svContent = sv(c)
+      cache.put(svelteExtenstionToJs(requestURL.pathname), jsMatch = newJavaScriptResponse(svContent))
+      return jsMatch
+    }))
+  } else if (event.request.mode === 'navigate') {
+    console.log('sw navigate ', event)
+    //const txt = event.request.url.substr(event.request.url.lastIndexOf('/'))
+    const value = 'aaa'//routes.some(r => r == txt) 
+    //? txt == '/' ? 'home' : txt.substr(1)
+    //: '404'
+
+    const html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset='utf-8'>
+      <meta name='viewport' content='width=device-width,initial-scale=1'>
+    
+      <title>Sevas</title>
+    
+      <link rel='icon' type='image/png' href='/favicon.png'>
+      <!--
+      <link rel='stylesheet' href='/global.css'>
+      <link rel='stylesheet' href='/components/header.css'>
+      <link rel='stylesheet' href='/components/${value}.css'>
+      <script defer src='/components/header.js'></script>
+      <script defer src='/components/${value}.js'></script>
+      https://unpkg.com/svelte@3.29.4/internal/index.mjs
+      -->
+      </head>
+      <body>
+      <script type="module">
+      import App from './App'
+      new App({target: document.body})
+      </script>
+    </body>
+    </html>
+    `
+    const headers = new Headers()
+    headers.append("Content-Type", "text/html")
+    const init = { "status" : 200 , "statusText": "OK", headers };
+    respondWith(event, new Response(html, init))
+  //} else if (assets.some(str => str == getValue(event.request.url))) {
+    //event.respondWith(caches.match(getValue(event.request.url)))
+  } else {
+    console.log('sw fetch ', event.request)
+    event.respondWith(fetch(event.request).then(response => {
+      console.log('sw fetch response', response)
+      return response//TODO cache response
+    }))
+  }
+});
--- packages/playground/src/index.ts
+++ packages/playground/src/index.ts
@@ -6,20 +6,54 @@ console.log('initializing', previewIframe)
 
 
 if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('./service-worker.js', {
-        scope: CONTROLLED
-    });
-    
-    navigator.serviceWorker.ready.then(() => {
-        console.log('serviceWorker ready')
-    }).catch(e => {
-        console.error('service worker error', e)
-    });
-    
-    if (navigator.serviceWorker.controller) {
-        console.log('sending ...')
-        navigator.serviceWorker.controller.postMessage({'hello': 'world'})
-    }
+    navigator.serviceWorker.getRegistrations().then(registrations => {
+        console.log('sw prev registrations', registrations)
+        const promises = registrations.map(registration => {
+            return registration.unregister()
+        })
+        return Promise.all(promises)
+    }, err => {
+        console.log('sw registration error ', err)
+    }).finally(() => {
+        navigator.serviceWorker.addEventListener('message', function(event) {
+            console.log('on sw listener message A', event)
+        });
+
+
+        function newResponse(content: string, contentType: string): Response {
+            const headers = new Headers()
+            headers.append("Content-Type", contentType)
+            const init = { "status" : 200 , "statusText": "OK", headers };
+            return new Response(content, init)
+        }
+
+        navigator.serviceWorker.register('./svelte-service-worker.js', {
+            scope: 'svelte/'
+        }).then(() => {
+            return navigator.serviceWorker.ready;
+        }).then(() => {
+            console.log('serviceWorker ready 1')
+        })
+
+        navigator.serviceWorker.register('./service-worker.js', {
+            scope: CONTROLLED
+        }).then(() => {
+            return navigator.serviceWorker.ready;
+        }).then(() => {
+            console.log('serviceWorker ready 1')
+        })
+        
+        navigator.serviceWorker.ready.then(() => {
+            console.log('serviceWorker ready 2')
+        }).catch(e => {
+            console.error('service worker error', e)
+        });
+        if (navigator.serviceWorker.controller) {
+            console.log('sending ...')
+            navigator.serviceWorker.controller.postMessage({'hello': 'world'})
+        }
+        console.log('sw AHOJ ', navigator.serviceWorker)
+    })
 } else {
     alert('NULL navigator.serviceWorker or navigator.serviceWorker.controller')
 }
@@ -33,8 +67,14 @@ if (refreshButton) {
             console.log('sw registration error ', err)
         })
 
+        
         if (previewIframe) {
+            previewIframe.contentWindow?.navigator?.serviceWorker?.addEventListener('message', function(event) {
+                console.log('on sw listener message B', event)
+            });
             previewIframe.src = CONTROLLED + 'index.html'
+        
+            console.log('XXXXXXXXXXXXXXXXXX', previewIframe.contentWindow?.navigator?.serviceWorker)
         }
     }
 }
