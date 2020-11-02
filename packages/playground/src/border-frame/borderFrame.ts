1dd7339e0f9babd2024e856ac97031556ea4f1c8
Martin Mecir <mecir.martin@gmail.com>
2020-11-02 09:38:22 +0100

    user is able to hover over frame

--- packages/playground/src/border-frame/borderFrame.ts
+++ packages/playground/src/border-frame/borderFrame.ts
@@ -1,13 +1,19 @@
 import { getOffset } from '../util/highlight';
 
 const iFrame = document.querySelector('iframe');
-console.log('hello world');
 
 const innerDoc = iFrame!.contentDocument || iFrame!.contentWindow!.document;
 
 innerDoc.addEventListener('mouseover', (e: MouseEvent) => {
   const styles = getOffset(e.target!);
   const mainDiv = innerDoc.getElementById('buttonContainer')!;
+
+  const icons = mainDiv.children[0].children[0].children;
+
+  for (let i = 0; i < icons.length; i++) {
+    if (icons[i] === e.target) return;
+  }
+
   mainDiv.setAttribute(
     'style',
     `height:${styles!.height}px; width:${
