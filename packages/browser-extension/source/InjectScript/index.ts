a83259075903ac47d345f180de405d980ca022e5
mecirmartin <mecir.martin@gmail.com>
2021-01-12 14:27:45 +0100

    added functionality to copy route

--- packages/browser-extension/source/InjectScript/index.ts
+++ packages/browser-extension/source/InjectScript/index.ts
@@ -1,5 +1,65 @@
+import { Project } from "ts-morph"
 import { DevTools } from "../devtools"
-import { tsClone } from "../tsx/clone"
+import { addCodeSnippet, findElementInCode, tsClone } from "../tsx/clone"
+
+const project = new Project()
+
+const createReactComponentTemplate = (name: string | null) => {
+  if (!name) return null
+
+  const reactComponent = project.createSourceFile(
+    `${name}.tsx`,
+    `import React from 'react'
+  
+      export default function Default() {
+        return (
+          <div>
+            Hello world
+          </div>
+        )
+      }`
+  )
+  const defaultFunction = reactComponent.getFunction("Default")
+  defaultFunction?.rename(name)
+  defaultFunction?.setBodyText(`return (<div>Hello ${name}</div>)`)
+
+  return reactComponent
+}
+
+const cloneElement = async (
+  code: string,
+  fileName: string,
+  columnNumber: number,
+  lineNumber: number
+) => {
+  const clonedCode = await tsClone(
+    code,
+    { columnNumber, fileName, lineNumber },
+    fileName
+  )
+  if (clonedCode) {
+    console.log("tu som")
+    const res = await fetch(`http://localhost:7500/files/${fileName}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "text/plain",
+      },
+      body: clonedCode,
+    })
+    console.log(await res.json())
+  }
+}
+
+const writeFile = async (fileName: string, code: string) => {
+  const res = await fetch(`http://localhost:7500/files/${fileName}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "text/plain",
+    },
+    body: code,
+  })
+  console.log(await res.text())
+}
 
 window.addEventListener("message", async event => {
   if (
@@ -16,17 +76,67 @@ window.addEventListener("message", async event => {
 
     const devTools = new DevTools(window.__REACT_DEVTOOLS_GLOBAL_HOOK__)
     const ownerList = devTools.getOwnersList(id)
-    if (ownerList && columnNumber && fileName && lineNumber) {
-      console.log("Mam vsetky", ownerList, columnNumber, fileName, lineNumber)
 
-      const res = await fetch(`http://localhost:7500/files/${fileName}`)
-      const code = await res.text()
-      const cloned = await tsClone(
+    const res = await fetch(`http://localhost:7500/files/${fileName}`)
+    const code = await res.text()
+
+    if (ownerList && columnNumber && fileName && lineNumber && code) {
+      console.log("Mam vsetky", {
+        ownerList,
+        columnNumber,
+        fileName,
+        lineNumber,
         code,
-        { columnNumber, fileName, lineNumber },
-        fileName
-      )
-      console.log(code, cloned)
+      })
+      if (ownerList[ownerList.length - 1].displayName === "Route") {
+        const routeCode = await findElementInCode(code, {
+          fileName,
+          columnNumber,
+          lineNumber,
+        })
+        if (!routeCode) return console.error("Element was not found in code")
+
+        // Prompt for new page name
+        const newPageName = window.prompt("New page name")
+        if (!newPageName) return
+
+        const newPath = `path="/${newPageName}"`
+        const newComponent = `component={${newPageName}}`
+        // Replace path attribute
+        let newRoute = routeCode.replace(
+          /path(\s*)=(\s*)("|'|`)(.*)("|'|`)/g,
+          newPath
+        )
+        // Replace component attribute
+        newRoute = newRoute.replace(/component(\s*)=(\s*){.*}/g, newComponent)
+        let newCode = await addCodeSnippet(
+          code,
+          { fileName, columnNumber, lineNumber },
+          newRoute
+        )
+
+        // TS-MORPH
+        if (newCode) {
+          // Add import with Ts-Morph
+          const alteredFile = project.createSourceFile("tryout.tsx", newCode)
+          alteredFile.addImportDeclaration({
+            defaultImport: newPageName,
+            moduleSpecifier: `./${newPageName}`,
+          })
+          writeFile(fileName, alteredFile.print()).catch(err =>
+            console.log(err)
+          )
+        }
+
+        const newReactComponent = createReactComponentTemplate(newPageName)
+
+        if (newReactComponent) {
+          writeFile(
+            `/Users/martinmecir/Desktop/Work/ionic-react-conference-app/src/${newPageName}.tsx`,
+            newReactComponent.print()
+          )
+        }
+      }
     }
   }
 })
--- packages/browser-extension/source/tsx/clone.ts
+++ packages/browser-extension/source/tsx/clone.ts
@@ -1,17 +1,58 @@
-import { SourceLineCol, astFindSource } from './ast'
+import { SourceLineCol, astFindSource } from "./ast"
 
-export async function tsClone(code: string, source: SourceLineCol, filePath: string) {
-  const found = await astFindSource(code, source);
+export const findElementInCode = async (
+  code: string,
+  source: SourceLineCol
+) => {
+  const found = await astFindSource(code, source)
+  if (found) {
+    const before = code.substring(0, found.end)
+
+    const startEndOfLine = before.lastIndexOf("\n")
+    const lineStarts = startEndOfLine >= 0 ? startEndOfLine : 0
+    return code.substring(lineStarts, found.end)
+  }
+  return null
+}
+
+export const addCodeSnippet = async (
+  code: string,
+  source: SourceLineCol,
+  codeSnippet: string
+) => {
+  const found = await astFindSource(code, source)
+  if (found) {
+    const before = code.substring(0, found.end)
+    const after = code.substring(found.end)
+
+    return before + codeSnippet + after
+  }
+
+  return null
+}
+
+export async function tsClone(
+  code: string,
+  source: SourceLineCol,
+  filePath: string
+) {
+  const found = await astFindSource(code, source)
   if (found) {
     if (found) {
-      console.log('source found', found, code.substring(found.pos, found.end), filePath);
+      console.log(
+        "source found",
+        found,
+        code.substring(found.pos, found.end),
+        filePath
+      )
     }
 
     const before = code.substring(0, found.end)
 
-    const startEndOfLine = before.lastIndexOf('\n')
-    const lineStarts = startEndOfLine >=0 ? startEndOfLine : 0
+    const startEndOfLine = before.lastIndexOf("\n")
+    const lineStarts = startEndOfLine >= 0 ? startEndOfLine : 0
     const toBeCloned = code.substring(lineStarts, found.end)
+    console.log(toBeCloned)
 
     const after = code.substring(found.end)
 
