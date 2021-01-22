7b51d7a25538d5157bc0179fdcba05398db7cb65
jslezak <jozef.slezak@gmail.com>
2021-01-22 11:00:13 +0100

    #45 generator table.ts

---
+++ packages/generator/src/tsx/aaa.ts
@@ -0,0 +1,9 @@
+// import * as ts from "typescript";
+// import { generateFactoryCode } from "ts-factory-code-generator-generator";
+
+// // get ast
+// const sourceFile = ts.createSourceFile("/file.ts", "4n + 5n;", ts.ScriptTarget.Latest);
+
+// // get the generated factory code
+// const factoryCode = generateFactoryCode(ts, sourceFile); // or provide a different node here
+// console.log(factoryCode);
