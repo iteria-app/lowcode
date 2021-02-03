211dd8c7455e51212e74deb358fecd0dffdb3c99
zadubanm02 <zadubanm02@gmail.com>
2021-02-03 08:23:45 +0100

    #44 tests

--- packages/browser-extension/source/localization/localizations.test.ts
+++ packages/browser-extension/source/localization/localizations.test.ts
@@ -1,69 +1,45 @@
 import ts, { ScriptKind, ScriptTarget } from "typescript"
 import { createAst } from "../tsx/createSourceFile"
 import { createTable } from "./localeTables"
-import { getValuesFromLocalizationASTJSON } from "./localizations"
+import { changeLocaleFile, extractAllStrings, getValuesFromLocalizationASTJSON } from "./localizations"
 import sk_SK from "./sk_SK"
 
 const expectedAST = createAst(JSON.stringify(sk_SK), ScriptTarget.ESNext, ScriptKind.JSON)
 const { english, slovak, locales, positionsTable } = getValuesFromLocalizationASTJSON(expectedAST)
+const originalWords = extractAllStrings(positionsTable)
+const positionsConst = positionsTable
 const positionLength = positionsTable.length
-const original: string = `export default {
-    "year": "rok",
-    "month": "mesiac",
-    "day": "den",
-    "minute": "minuta",
-    "second": "sekunda",
-    "new": "novy",
-    "old": "starky",
-    "super": "super" }`
+//change this array for making changes of final result json
+// i.e. change text property
+const positionsChanged = [{ "text": "year", "start": 1, "end": 7 }, { "text": "xxxx", "start": 8, "end": 13 }, { "text": "month", "start": 14, "end": 21 }, { "text": "mesiac", "start": 22, "end": 30 }, { "text": "day", "start": 31, "end": 36 }, { "text": "den", "start": 37, "end": 42 }, { "text": "minute", "start": 43, "end": 51 }, { "text": "xxx", "start": 52, "end": 60 }, { "text": "second", "start": 61, "end": 69 }, { "text": "sekunda", "start": 70, "end": 79 }, { "text": "new", "start": 80, "end": 85 }, { "text": "novy", "start": 86, "end": 92 }, { "text": "old", "start": 93, "end": 98 }, { "text": "starky", "start": 99, "end": 107 }, { "text": "super", "start": 108, "end": 115 }, { "text": "super", "start": 116, "end": 123 }]
+const row = '"year":"xxxx"'
 
-const printer = ts.createPrinter({
-    newLine: ts.NewLineKind.LineFeed,
+it('compare positions should not be the same after change', () => {
+    const astLocale = createAst(JSON.stringify(sk_SK), ScriptTarget.ESNext, ScriptKind.JSON)
+    const { english, slovak, locales, positionsTable, englishPositionsTable } = getValuesFromLocalizationASTJSON(astLocale)
+    const originalWords = extractAllStrings(positionsTable)
+    expect(positionsTable).not.toBe(positionsChanged)
 })
 
-// it("positions should be array", () => {
-//     const createdAST = createAst(JSON.stringify(sk_SK), ScriptTarget.ESNext, ScriptKind.JSON)
-//     const { english, slovak, locales, positionsTable } = getValuesFromLocalizationASTJSON(createdAST)
-//     expect(positionsTable).toHaveLength(positionLength)
-// })
+it('compare positions table to be equal after function', () => {
+    const astLocale = createAst(JSON.stringify(sk_SK), ScriptTarget.ESNext, ScriptKind.JSON)
+    const { english, slovak, locales, positionsTable, englishPositionsTable } = getValuesFromLocalizationASTJSON(astLocale)
+    expect(positionsTable).toEqual(positionsConst)
+})
 
-// it("Test if objects are equal", () => {
-//     const createdAST = createAst(JSON.stringify(sk_SK), ScriptTarget.ESNext, ScriptKind.JSON)
-//     const { english, slovak, locales, positionsTable } = getValuesFromLocalizationASTJSON(createdAST)
-//     const transformedAST = transformSourceFile(positionsTable)
-//     const updatedFile = createFinalFile(printer, transformedAST.compilerNode)
-//     if (updatedFile) {
-//         let replaced = replaceCommaLine(updatedFile)
-//         replaced = 'export default ' + replaced;
-//         console.log("replaced", replaced, typeof (replaced))
-//         expect(replaced.replace(/\s/g, '')).toEqual(original.replace(/\s/g, ''));
-//     }
-// })
+it('compare if changed locale contains changed text', () => {
+    const astLocale = createAst(JSON.stringify(sk_SK), ScriptTarget.ESNext, ScriptKind.JSON)
+    const { english, slovak, locales, positionsTable, englishPositionsTable } = getValuesFromLocalizationASTJSON(astLocale)
+    const resultOfChanging = changeLocaleFile(JSON.stringify(sk_SK), positionsChanged, originalWords)
+    expect(resultOfChanging).toContain('xxx')
+})
 
-// it("If final string cointains", () => {
-//     const createdAST = createAst(JSON.stringify(sk_SK), ScriptTarget.ESNext, ScriptKind.JSON)
-//     const { english, slovak, locales, positionsTable } = getValuesFromLocalizationASTJSON(createdAST)
-//     const transformedAST = transformSourceFile(positionsTable)
-//     const updatedFile = createFinalFile(printer, transformedAST.compilerNode)
-//     if (updatedFile) {
-//         let replaced = replaceCommaLine(updatedFile)
-//         replaced = 'export default ' + replaced;
-//         expect(replaced).toContain('mesiac');
-//     }
-// })
 
-// const changedPositions = [{ "text": "rok", "start": 8 }, { "text": "mesic", "start": 22 }, { "text": "den", "start": 37 }, { "text": "minuta", "start": 52 }, { "text": "sekunda", "start": 70 }, { "text": "novy", "start": 86 }, { "text": "starky", "start": 99 }, { "text": "super", "start": 116 }]
-
-// it("Test changed slovak translation", () => {
-//     const createdAST = createAst(JSON.stringify(sk_SK), ScriptTarget.ESNext, ScriptKind.JSON)
-//     const { english, slovak, locales, positionsTable } = getValuesFromLocalizationASTJSON(createdAST)
-//     const transformedAST = transformSourceFile(changedPositions)
-//     const updatedFile = createFinalFile(printer, transformedAST.compilerNode)
-//     if (updatedFile) {
-//         let replaced = replaceCommaLine(updatedFile)
-//         replaced = 'export default ' + replaced;
-//         expect(replaced).toContain('mesic');
-//     }
-// })
-
+it('compare if the row has changed row', () => {
+    const astLocale = createAst(JSON.stringify(sk_SK), ScriptTarget.ESNext, ScriptKind.JSON)
+    const { english, slovak, locales, positionsTable, englishPositionsTable } = getValuesFromLocalizationASTJSON(astLocale)
+    const resultOfChanging = changeLocaleFile(JSON.stringify(sk_SK), positionsChanged, originalWords)
+    const firstRow = resultOfChanging.substring(1, 14)
+    expect(firstRow).toEqual(row)
+})
 
