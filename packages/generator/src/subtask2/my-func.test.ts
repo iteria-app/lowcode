053cd39e81d7d684817107c088d35adf09f4db3d
Elxan123 <elxan.aslanov2000@gmail.com>
2021-02-24 00:59:12 +0400

    Table - add column traformation Updated as You Want

--- packages/generator/src/subtask2/my-func.test.ts
+++ packages/generator/src/subtask2/my-func.test.ts
@@ -1,33 +1,23 @@
 import {createNewColumn} from './my-func';
 import ts from 'typescript';
 test('my own test', () => {
-    createNewColumn(` 
+    const newFile = createNewColumn(` 
 
-    import{ aaa, asd,TableColumn} from 'dsada';
-    import{ ss} from 'dsada';
+    import{ TableRow, TableCell } from 'table-components';
+    import{ ts } from 'type-script';
     
     
 
-    <Table>
-    <TableRow>
-    <TableCell>
-    <FormattedMessage id="Parent.created_at"/>
-    </TableCell><TableCell>
-    <FormattedMessage id="Parent.date1"/>
-    </TableCell><TableCell>
-    <FormattedMessage id="Parent.double1"/></TableCell><TableCell><FormattedMessage id="Parent.flag1"/>
-    </TableCell><TableCell>
-    <FormattedMessage id="Parent.id"/></TableCell><TableCell><FormattedMessage id="Parent.money1"/>
-    </TableCell><TableCell><FormattedMessage id="Parent.numeric1"/></TableCell><TableCell>
-    <FormattedMessage id="Parent.seq"/></TableCell><TableCell><FormattedMessage id="Parent.time1"/>
-    </TableCell><TableCell><FormattedMessage id="Parent.title"/>
-    </TableCell><TableCell><FormattedMessage id="Parent.uuid1"/></TableCell>
-    </TableRow>{parents.map(parent => <TableRow><TableCell>created_at{parent.created_at}</TableCell>
-        <TableCell>date1{parent.date1}</TableCell><TableCell>double1<FormattedNumber value={parent.double1}/>
-        </TableCell><TableCell>flag1{parent.flag1}</TableCell><TableCell>id{parent.id}</TableCell>
-        <TableCell>money1{parent.money1}</TableCell><TableCell>numeric1<FormattedNumber value={parent.numeric1}/>
-        </TableCell><TableCell>seq{parent.seq}</TableCell><TableCell>time1{parent.time1}</TableCell>
-        <TableCell>title{parent.title}</TableCell>
-        <TableCell>uuid1{parent.uuid1}</TableCell></TableRow>)}
+    <Table property="">
+        <TableRow>
+            <TableCell>A cell</TableCell>
+        </TableRow>
+        <TableRow>
+            <TableCell>
+                My data
+            </TableCell>
+        </TableRow>
     </Table>`,'New Column');
+    const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed });
+    console.log(printer.printNode(ts.EmitHint.Unspecified,newFile,newFile))
 })
--- packages/generator/src/subtask2/my-func.ts
+++ packages/generator/src/subtask2/my-func.ts
@@ -1,9 +1,5 @@
-import { exit } from 'process';
-import { Project, SourceFile } from 'ts-morph';
-import { ts } from 'ts-morph';
-import { factory, forEachChild } from 'typescript';
+import ts,{ factory } from 'typescript';
 import { TableColumn } from './column';
-// import { column } from './../../../../../Learning/ReactJS/task1/src/subtask2/column';
 import { createJsxElement } from './component';
 
 
@@ -25,50 +21,39 @@ function createAst(
       scriptKind
     )
   }
-export function createNewColumn(file: string, newColumnName: string): void {
 
-    const sourceFile = createAst(file);
-    const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed });
-
-    
-    let isImported:boolean = false 
-    function importCheck(nodes:ts.Node[]|ts.Node) {
-      ts.forEachChild(sourceFile, node => {
+const importCheck = (SourceFile:ts.SourceFile|ts.Node) => {
+      return <boolean>ts.forEachChild(SourceFile, node => {
         if(ts.isImportDeclaration(node)){
           let temparr = node.importClause?.namedBindings?.getChildren()[1].getChildren().map((n) => n.getText()=='TableColumn'?true:false) 
           if (temparr && temparr.indexOf(true) > -1){
-            isImported = true
+            return true
           }
         }
       })
-    }
-    importCheck(sourceFile)
-    
+}  
+
+export function createNewColumn(file: string, newColumnName: string): ts.SourceFile {
+    const sourceFile = createAst(file);
+    let isImported:boolean = importCheck(sourceFile)
     const transformer: ts.TransformerFactory<ts.SourceFile> = context => {
       return sourceFile => {
         const visitor = (node: ts.Node): ts.VisitResult<ts.Node> => {
-          
           if(!isImported){
             if(ts.isImportDeclaration(node)){
               isImported = true
               return [TableColumn.importDeclaration,node]
             }
-           
           }
-
-          if(ts.isJsxElement(node) && node.getChildAt(0).getText() === '<Table>'){
+          if(ts.isJsxElement(node) && node.openingElement.tagName.getText() === 'TableRow'){
             return factory.updateJsxElement(node,node.openingElement,[createJsxElement(TableColumn,[],[factory.createJsxText(newColumnName)]),...node.children],node.closingElement)
           }
-
           return ts.visitEachChild(node,visitor,context)
         }
-
         return ts.visitNode(sourceFile,visitor)
       }
     }
-    const myOutput = ts.transform(sourceFile,[transformer]).transformed[0]
-    
-    console.log(printer.printNode(ts.EmitHint.Unspecified, myOutput, myOutput))
+    return ts.transform(sourceFile,[transformer]).transformed[0]
 }
 
     
