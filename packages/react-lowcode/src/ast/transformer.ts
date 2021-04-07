1f78b37dad97dd8ac3c1e7ae522e47f287d47ef5
mecirmartin <mecir.martin@gmail.com>
2021-04-07 07:07:24 +0200

    added removeElementInAst

--- packages/react-lowcode/src/ast/transformer.ts
+++ packages/react-lowcode/src/ast/transformer.ts
@@ -20,11 +20,31 @@ const addTransformer = <T extends ts.Node, U extends ts.Node | ts.JsxChild>(
   }
 }
 
+const removeTransformer = <T extends ts.Node>(
+  start: number
+): ts.TransformerFactory<T> => {
+  return context => {
+    const visit: ts.Visitor = node => {
+      const nodeStart = node.pos
+      if (nodeStart === start) return
+
+      return ts.visitEachChild(node, child => visit(child), context)
+    }
+
+    return node => ts.visitNode(node, visit)
+  }
+}
+
 export const addElementsToAST = <T extends ts.Node>(
   ast: ts.SourceFile,
   start: number,
   newNodes: Array<T>
 ) => {
   const result = ts.transform(ast, [addTransformer(start, newNodes)])
+  return result.transformed[0]
+}
+
+export const removeElementFromAst = (ast: ts.SourceFile, start: number) => {
+  const result = ts.transform(ast, [removeTransformer(start)])
   return result.transformed[0]
 }
