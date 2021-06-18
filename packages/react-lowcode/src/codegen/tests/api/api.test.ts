d6d91a9074807baddc623c50de0e4952e651881a
jslezak <jozef.slezak@gmail.com>
2021-06-18 09:04:56 +0200

    typename in addColumn(), addFormInput() options

--- packages/react-lowcode/src/codegen/tests/api/api.test.ts
+++ packages/react-lowcode/src/codegen/tests/api/api.test.ts
@@ -26,14 +26,14 @@ describe(".api tests", () => {
         const filePath = 'src/codegen/tests/list/files/is-datatable-test-file.txt'
         const source : SourceLineCol = {lineNumber: 12, columnNumber:17, fileName:filePath}
 
-        addColumn(graphqlGenTs1, new CodegenRw(), source, {property: 'testdate'}).then(generated => console.log(generated))
+        addColumn(graphqlGenTs1, new CodegenRw(), source, {property: 'testdate', entity: 'Customer'}).then(generated => console.log(generated))
     });
 
     test(".add form input", () => {
         const filePath = 'src/codegen/tests/detail/detail-test-file.txt'
         const source : SourceLineCol = {lineNumber: 69, columnNumber:17, fileName:filePath}
 
-        addFormInput(graphqlGenTs1, new CodegenRw(), source, {property: 'test2'}).then(generated => console.log(generated))
+        addFormInput(graphqlGenTs1, new CodegenRw(), source, {property: 'test2', entity: 'Customer'}).then(generated => console.log(generated))
     });
 
     test(".delete column", () => {
