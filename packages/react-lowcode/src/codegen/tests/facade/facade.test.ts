089816ebef2451eb06a28f77787317a5275e01da
Ing. Peter Čunderlík <peter.cunderlik@afsi.com>
2021-06-20 16:25:00 +0200

    Code polishing 13.6.2021

--- packages/react-lowcode/src/codegen/tests/facade/facade.test.ts
+++ packages/react-lowcode/src/codegen/tests/facade/facade.test.ts
@@ -2,14 +2,38 @@ import { sourceFileEntity, getEntityProperty, parseGraphqlTypes } from "../helpe
 import { graphqlGenTs1 } from "../typeAlias.example";
 import { deleteColumn, insertColumn, insertColumnToBasicTableGrommet, insertColumnToBasicTableMui, insertColumnToDataTableGrommet, insertFormWidget } from "../../facade/facadeApi";
 import { CodegenRw } from "../../io/codegenRw";
+import { SourceLineCol } from "../../../ast";
+import { FacadeInsertOptions } from "../../facade/interfaces";
+import { TestListHelper } from "../list/list-helper";
 
-describe("codegen facade", () => {
+describe("codegen facade tests", () => {
+  test(".add column (MUI DataTable) (ReactIntl)", async () => {
+    const tablePosition: SourceLineCol = {
+      lineNumber: 15,
+      columnNumber: 73,
+      fileName: 'src/codegen/tests/facade/files/mui/data-table/react-intl/add-column.txt'
+    };
+    const options: FacadeInsertOptions = {
+      entityField: getEntityProperty(graphqlGenTs1, 'testdate')[0],
+      index: 1
+    };
 
-    test(".add column to existing table", () => {
-      insertColumn({lineNumber: 15,columnNumber: 73, fileName: 'src/codegen/tests/list/list-test-file.txt'}, {entityField: getEntityProperty(graphqlGenTs1, 'testdate')[0], index:8}, new CodegenRw()).then(
-        (data) => console.log(data)
-      )
-    });
+    // TODO:PC: in result is: <FormattedMessage id=".testdate" defaultMessage="testdate"/> 
+    // id should be: customer.testdate
+    // need to create a test for check this
+    const result = await insertColumn(tablePosition, options, new CodegenRw());
+    const position = TestListHelper.getMuiDataTablePosition(result);
+    const columnNames = TestListHelper.getMuiDataTableColumnNames(result, position);
+
+    // TODO:PC: Index setting doesn't seem to be taken into account (column always inserted at the end)
+    expect(columnNames).toStrictEqual([
+      'testdate',
+      'id',
+      'name',
+      'createdAt',
+      'email'
+    ]);
+  });
 
     test(".delete column from existing table", () => {
       deleteColumn({lineNumber: 15,columnNumber: 73, fileName: 'src/codegen/tests/list/list-test-file.txt'}, { index:5 }, new CodegenRw()).then(
