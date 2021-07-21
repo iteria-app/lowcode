7f0bd95267d31ea3353caf4bc31d241ec698a783
unknown <yadibolt@gmail.com>
2021-07-21 18:43:53 +0200

    Added simplecards

---
+++ packages/templates/react-urql-graphql-codegen/src/components/Simplecard.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+export default function SimpleCard(customer_id: number, customer_name: string) {
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
+
+  return (
+    <Card className={classes.root}>
+      <CardContent>
+        <Typography variant="h5" component="h2">
+          {customer_name}
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          {customer_id}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
