ed38fafccbeacef53f686332d77c6eeef7e057b3
Ing. Peter Čunderlík <peter.cunderlik@afsi.com>
2021-08-04 20:51:02 +0200

    Code polishing 13.6.2021

--- packages/react-lowcode/src/codegen/tests/detail/detail.test.ts
+++ packages/react-lowcode/src/codegen/tests/detail/detail.test.ts
@@ -4,14 +4,16 @@ import { graphqlGenTs1 } from "../typeAlias.example"
 import { UiFramework, TableType, Formatter } from '../../definition/context-types'
 import { AppGenerator } from '../../generation/generators/app-generator'
 import { sourceFileEntity, createAst, parseGraphqlTypes } from "../helper"
+import { createEntityFromIntrospection, Entity } from "../../generation/entity"
+import { is2 } from "../introspection-example"
 
   test(".mui formik generation", () => {
-      const sourceFile = createAst('')
-      const myClassFile = parseGraphqlTypes(graphqlGenTs1)
-      const testEntity = sourceFileEntity(myClassFile)
+      const sourceFile = createAst('');
+      const entityName = 'customer';      
+      const entity: Entity | undefined = createEntityFromIntrospection(is2.data.__schema, entityName);
 
-      let generationContext = {formatter: Formatter.ReactIntl, uiFramework: UiFramework.MaterialUI, tableType: TableType.BasicTable, entity: testEntity!!};
-      let generator = new AppGenerator(generationContext , testEntity!!);
+      let generationContext = {formatter: Formatter.ReactIntl, uiFramework: UiFramework.MaterialUI, tableType: TableType.BasicTable, entity: entity!!};
+      let generator = new AppGenerator(generationContext, entity!!);
 
       const page = generator.generateDetailPage()
       
